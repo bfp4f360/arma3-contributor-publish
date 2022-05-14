337efb0aa0e92a0c1f6c15476e1a5313d453f4f3
Namenai <bfp4f360@gmail.com>
2022-05-14 05:01:21 -0500

    form redone

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,30 @@
 import React from 'react';
 import './App.css';
 
-import { Link } from "react-router-dom";
-import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
-import Stack from '@mui/material/Stack';
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
-import TextField from '@mui/material/TextField';
-
-interface IFormInput {
-  modPath: string;
-  modId: string;
-  changelogFile: string;
-}
+import PublishModForm from './components/publishModForm';
 
 
-const App = () => {
-  const { control, handleSubmit } = useForm<IFormInput>();
-
-  const onSubmit: SubmitHandler<IFormInput> = data => {
-    console.log(data)
-  };
-
+export default function App() {
   return (
-    <div className="App" style={{"display":"flex","justifyContent":"center","paddingTop":"10vh"}}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack spacing={2} direction="column">
-          <Stack spacing={2} direction="row">
-            <Controller
-              name="modId"
-              control={control}
-              defaultValue=""
-              render={({ field }) => <TextField label="Mod Steam ID" {...field} />}
-            />
-            <Controller
-              name="changelogFile"
-              control={control}
-              defaultValue=""
-              render={({ field }) => <TextField label="Change Log File Path" {...field} />}
-            />
-          </Stack>
-          <Controller
-            name="modPath"
-            control={control}
-            defaultValue=""
-            render={({ field }) => <TextField label="Mod Addon Folder Path" {...field} />}
-          />
-          <div style={{"display":"flex","justifyContent":"space-between"}}>
-            <Button variant="contained">Publish</Button>
-            <Link to="/Settings" style={{"textDecoration":"none"}}>
-              <Button variant="contained" color='secondary' disabled>Settings</Button>
-            </Link>
-          </div>
-        </Stack>
-      </form>
-    </div>
-  );
-};
-
-export default App;
+    <React.Fragment>
+      <div style={{display:'flex',alignItems:'center',justifyContent:"center","paddingTop":"10vh"}}>
+        <PublishModForm/>
+      </div>
+      
+    {/* <Grid container spacing={2} direction="column" justifyContent="center" alignItems="center">
+      <Grid item lg={1}>
+        <PublishModForm/>
+      </Grid> */}
+      {/* <Grid item lg={3}>
+        <PublishModForm/>
+      </Grid> */}
+    {/* </Grid> */}
+    
+    </React.Fragment>
+  )
+}
---
+++ src/components/publishModForm.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import '../App.css';
+
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+import { styled } from '@mui/material/styles';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+
+
+
+interface IFormInput {
+  modPath: string;
+  modId: string;
+  changelogFile: string;
+}
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
+const PublishModForm = () => {
+  const { control, handleSubmit } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = data => {
+    console.log(data)
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box sx={{ flexGrow: 1}}>
+        <Grid container spacing={2} >
+          {/* Row 1 */}
+          <Grid container item  style={{display:"flex",justifyContent:"center"}}>
+            <React.Fragment>
+              <Grid item xs={4}>
+                <Controller
+                  name="modId"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => <TextField type="number" label="Mod Steam ID" {...field} />}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Controller
+                  name="changelogFile"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => <TextField fullWidth  label="Change Log File Path" {...field} />}
+                />
+              </Grid>
+            </React.Fragment>
+          </Grid>
+
+          {/* row 2 */}
+          <Grid container item  style={{display:"flex",justifyContent:"center"}}>
+            <React.Fragment>
+              <Grid item xs={8} >
+                <Controller
+                  name="modPath"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => <TextField fullWidth label="Mod Addon Folder Path" {...field} />}
+                />
+              </Grid>
+            </React.Fragment>
+          </Grid>
+
+          {/* row 3 */}
+          <Grid container item  style={{display:"flex",justifyContent:"center"}}>
+            <React.Fragment>
+              <Grid item xs={8} style={{display:"flex",justifyContent:'space-between'}}>
+                <Button variant="contained" type="submit">Publish</Button>
+                <Link to="/Settings" style={{"textDecoration":"none"}}>
+                    <Button variant="contained" color='secondary' disabled>Settings</Button>
+                </Link>
+              </Grid>
+            </React.Fragment>
+          </Grid>
+        </Grid>
+      </Box>
+    </form>
+
+    
+  );
+};
+
+export default PublishModForm;
